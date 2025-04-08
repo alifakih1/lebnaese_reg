1d3b9b8386d46c4667ca8f5e11ec4dd57c6066d6
alifakih1 <mr.fakih@gmail.com>
2025-04-08 08:11:35 +0000

    updates on setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Setup file for lebanese_regulations package.
+This is a minimal setup.py file that defers to pyproject.toml for configuration.
+"""
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
